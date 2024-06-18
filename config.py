a805b85cc7c5b88a239bfeff9c490108ddd125a2
Paizac <30595111+Paizac@users.noreply.github.com>
2024-06-19 00:55:07 +0200

    Ukonczenie proejktu

--- config.py
+++ config.py
@@ -3,15 +3,14 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 base_dir = Path(__file__).resolve().parent
-#Przechowywanie sciezki do pliku dotenv
+# Przechowywanie ścieżki do pliku dotenv
 env_file = base_dir / '.env'
-load_dotenv(env_file)  #załadowanie ustawień z pliku
+load_dotenv(env_file)  # załadowanie ustawień z pliku
 
 
-#Klasa odpowiedzialna za przechowywanie ustawien aplikacji
-
+# Klasa odpowiedzialna za przechowywanie ustawień aplikacji
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PER_PAGE = 5
@@ -19,7 +18,7 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///' + str(base_dir / 'development.db'))
 
 
 class TestingConfig(Config):
@@ -29,7 +28,16 @@ class TestingConfig(Config):
     TESTING = True
 
 
+class ProductionConfig(Config):
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_USERNAME = os.getenv('DB_USERNAME', 'root')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+    DB_NAME = os.getenv('DB_NAME', 'production_db')
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4'
+
+
 config = {
     'development': DevelopmentConfig,
-    'testing': TestingConfig
+    'testing': TestingConfig,
+    'production': ProductionConfig
 }
